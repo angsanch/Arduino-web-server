31998174db00b2444c235baf38aa59e416e3958a
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-07-02 21:52:49 +0200

    Check if tag exists

--- processing.ino
+++ processing.ino
@@ -1,3 +1,27 @@
+class Args {
+  private:
+    String _input;
+
+  public:
+    void setInput (String args) {
+      _input = args;
+    }
+
+    boolean exists (String tag) {
+      int index = _input.indexOf (tag);
+      if (index != -1) {
+        if (index == 0 || _input.charAt (index - 1 == '&')) {
+          if (_input.charAt (index + tag.length ()) == '=') {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+};
+
 void process (EthernetClient client, String path, String arguments) {
   hostFile ("index.htm", client);
+  Args args;
+  args.setInput (arguments);
 }
