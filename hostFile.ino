62b7fb374eb4b803d3b15d9d741b9d7936c155d2
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-07-24 20:56:20 +0200

    Moved header send to hoster class

--- hostFile.ino
+++ hostFile.ino
@@ -1,13 +1,30 @@
 class Hoster {
   private:
     File32 hosted;
-    String filename;
     EthernetClient client;
+    String status_code;
+    String content_type;
+    String filename;
 
   public:
-    Hoster (String fileName, EthernetClient eclient, String basePath = "htdocs") {
-      filename = basePath + fileName;
+    Hoster (EthernetClient eclient, String estatus_code, String econtent_type, String fileName, String basePath = "htdocs") {
       client = eclient;
+      status_code = estatus_code;
+      content_type = econtent_type;
+      filename = basePath + fileName;
+    }
+
+    void sendHeader () {
+      String status_message = sd.open ("/info/codes/" + status_code).readStringUntil ('\n');
+      hosted = sd.open ("/info/templates/header.txt");
+      while (hosted.available () > 0) {
+        String line = hosted.readStringUntil ('\n');
+        line.replace ("~sc~", status_code);
+        line.replace ("~sm~", status_message);
+        line.replace ("~ct~", content_type);
+        client.println (line);
+      }
+      client.println ();
     }
 
     void hostFile () {
@@ -25,22 +42,10 @@ class Hoster {
 
 File32 file;
 
-void sendHeader (EthernetClient client, String status_code, String content_type) {
-  String status_message = sd.open ("/info/codes/" + status_code).readStringUntil ('\n');
-  file = sd.open ("/info/templates/header.txt");
-  while (file.available () > 0) {
-    String line = file.readStringUntil ('\n');
-    line.replace ("~sc~", status_code);
-    line.replace ("~sm~", status_message);
-    line.replace ("~ct~", content_type);
-    client.println (line);
-  }
-  client.println ();
-}
-
 void response (EthernetClient client, String filename, String status_code) {
-  sendHeader (client, status_code, getContentType (filename.substring (filename.lastIndexOf ('.') + 1, filename.length ())));
-  Hoster (filename, client).hostFile ();
+  Hoster host (client, status_code, getContentType (filename.substring (filename.lastIndexOf ('.') + 1, filename.length ())), filename);
+  host.sendHeader ();
+  host.hostFile ();
 }
 
 String getContentType (String fileFormat) {
